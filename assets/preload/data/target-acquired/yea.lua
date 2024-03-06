3864a25fb0aa01ea18ea519f6797494011bc0a20
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-03-06 11:15:28 +0200

    Add files via upload

---
+++ assets/preload/data/target-acquired/yea.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+	setPropertyFromGroup('opponentStrums', 0, 'x', 6900);
+	setPropertyFromGroup('opponentStrums', 1, 'x', 6900);
+	setPropertyFromGroup('opponentStrums', 2, 'x', 6900);
+	setPropertyFromGroup('opponentStrums', 3, 'x', 6900);
+end
